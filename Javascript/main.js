7b189a2acfa5f476104da33012fb1ed59d5e58a3
Palutzii <pavelsarmiento@hotmail.com>
2022-12-12 20:40:46 +0100

    ADD: Swipper for reviews

---
+++ Javascript/main.js
@@ -0,0 +1,42 @@
+// Imports Swiper
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+
+// Menu trigger
+var trigger = document.querySelector(".nav-mobile-trigger");
+var navMobile = document.querySelector(".nav-mobile");
+if (trigger) {
+  trigger.addEventListener("click", function (el) {
+    if (trigger.classList.contains("is-clicked")) {
+      trigger.classList.remove("is-clicked");
+      navMobile.classList.remove("is-open");
+    } else {
+      trigger.classList.add("is-clicked");
+      navMobile.classList.add("is-open");
+    }
+  });
+}
+
+var swiper = document.querySelector(".swiper");
+if (swiper) {
+  // init Swiper
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    centeredSlider: false,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
